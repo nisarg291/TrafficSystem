f580dc25fb57464d9d1f4c2135a60f914f453174
Nisarg291 <59527108+nisarg291@users.noreply.github.com>
2023-03-26 21:08:09 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".toggle-password").click(function() {
+
+        $(this).toggleClass("eye");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+      });
+})
